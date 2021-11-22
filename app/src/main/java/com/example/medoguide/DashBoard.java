953dd16465eeb63ce5b47fa8d65c5e6606ebb2d3
Sourav Sahoo <jackysourav1994@gmail.com>
2021-11-22 18:30:07 +0530

    minor updation

--- app/src/main/java/com/example/medoguide/DashBoard.java
+++ app/src/main/java/com/example/medoguide/DashBoard.java
@@ -8,16 +8,22 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.medoguide.Model.Doctors;
 import com.example.medoguide.Model.Users;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class DashBoard extends AppCompatActivity {
 
     TextView username;
+    private FirebaseUser firebaseUser;
+    private DatabaseReference ref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,14 +36,32 @@ public class DashBoard extends AppCompatActivity {
         Button chat = findViewById(R.id.chat);
         Button settings = findViewById(R.id.settings);
 
-        FirebaseDatabase.getInstance().getReference().child("users").child("patients")
-                .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        ref = FirebaseDatabase.getInstance().getReference().child("users");
+
+        ref.child("patients").child(firebaseUser.getUid()).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                /*for(DataSnapshot dataSnapshot : snapshot.getChildren()){
-                }*/
+
+                try {
                     Users users = snapshot.getValue(Users.class);
                     username.setText(String.format("Hi %s", users.getName()));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    ref.child("doctors").child(firebaseUser.getUid()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            Doctors doctors = snapshot.getValue(Doctors.class);
+                            username.setText(String.format("Hi %s", doctors.getName()));
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
             }
 
             @Override
